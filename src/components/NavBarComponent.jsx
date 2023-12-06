1af749afa7d6db7b3fd094e979a9868ff98d25bf
paolodevprojects <paolo.charca@processcontrol.es>
2023-12-06 14:15:04 -0500

    [ADD] react and and style base for portafolio

---
+++ src/components/NavBarComponent.jsx
@@ -0,0 +1,10 @@
+const NavBarComponent = () => {
+    return (
+        <header className="container">
+            <h1>Paolo ChM Dev</h1>
+
+        </header>
+    )
+}
+
+export default NavBarComponent;
